43040c3267b23c6cc0317d5bfa42a23dff8f478c
BenjaminSnyder <0701benjamin@gmail.com>
2020-02-08 17:54:15 -0500

    Skeleton code for dataset file created.

---
+++ Dataset.py
@@ -0,0 +1,16 @@
+class Dataset:
+    __name__() = 
+
+    #supplies the test and train data in the format
+    def load_data():
+        
+        return ((x_train, y_train),(x_test, y_test))
+    #returns a string of the dataset name
+    def get_name():
+        pass
+    #returns an integer of the proper batchsize for the dataset
+    def get_batch_size():
+        pass
+    #returns an integer of the proper epoch count for the dataset
+    def get_epoch_count():
+        pass
