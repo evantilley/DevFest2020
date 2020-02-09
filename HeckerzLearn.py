165c10500db2ab47fb83df991ee87bed80e4f2cd
evantilley <elt2141@clac.cs.columbia.edu>
2020-02-09 09:31:22 -0500

    Finally read the CSV files :')

--- HeckerzLearn.py
+++ HeckerzLearn.py
@@ -1,5 +1,6 @@
 import numpy as np
-import pandas as pd
+import pandas as pandas
+import pymongo
 from sklearn import linear_model
 import sklearn
 from sklearn.utils import shuffle
@@ -7,82 +8,119 @@ from pymongo import MongoClient
 from pprint import pprint
 import pickle
 import json
+import csv
+import time
 
 # TODO: have node js add csv and json to
 #  directory then execute script
 
 def main():
+    while (1):
+        client = pymongo.MongoClient('mongodb://localhost:27017/')
+        db = client["MachineLearning"]
+        collection1 = db["Data"]
+        while collection1.count() < 1:
+            pass
+        time.sleep(1)
+        x = collection1.find_one()
 
-    client = MongoClient('mongodb://localhost:27017/MachineLearning')
-    db = client.admin
+        collection2 = db[x['dataset']]
+        #print(collection2.find("G1"))
 
-    serverStatusResult = db.command("serverStatus")
-    pprint(serverStatusResult)
+        # serverStatusResult = db.command("serverStatus")
+        # pprint(serverStatusResult)
 
-    dataset = serverStatusResult['dataset']
-    attributes = serverStatusResult['attributes']
-    predict = serverStatusResult['predict']
-    attributes.append(predict)
+        dataset = x['dataset']
+        attributes = x['attributes']
+        predict = x['predict']
+        attributes.append(predict)
 
-    # print(dataset)
+        cursor = collection2.find()
+        mongo_docs = list(cursor)
+        docs = pandas.DataFrame(columns = [])
+        for num, doc in enumerate(mongo_docs):
+            doc["_id"] = (doc["_id"])
+            doc_id = doc["_id"]
+            series_obj = pandas.Series(doc,name = doc_id)
+            docs = docs.append(series_obj)
+
+        docs.to_csv("susling.csv")
 
 
-    # with open('DevFest2020/input.json') as f:
-    #     input = json.load(f)
 
-    # dataset = input['dataset']
-    # attributes = input['attributes']
-    # predict = input['predict']
-    # attributes.append(predict)
+        f = open("ha.csv", "wt")
+        with open("susling.csv", 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for line in csv_reader:
+                for i in line:
+                    f.write(str(i))
+                    f.write('\n')
 
-    # dataset = "student-mat.csv"
-    # attributes = ["G1", "G2", "absences", "failures", "studytime", "G3"]
-    # predict = "G2"
+        count1 = 0
+        count2 = 1
+        test = False
+        f = open("ha.csv", "r").readlines()
+        with open("final.csv", "w") as outfile:
+            for index, line in enumerate(f):
+                # if (index != 1 and index != 3 and index != 4 and index != 6 and index != 7 and index != 9 and index != 10 and index != 12 and index != 13):
+                #     outfile.write(line)
+                if (index == 1):
+                    pass
+                if (index != count1 and index != count2):
+                    outfile.write(line)
+                    count1 += 3
+                    count2 += 3
 
-    data = pd.read_csv(dataset, sep=";")
-    data = data[attributes]
-    data = shuffle(data)
 
-    x = np.array(data.drop([predict], 1))
-    y = np.array(data[predict])
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-    best = 0
-    for _ in range(20):
+        f = open("final.csv")
+        data = pandas.read_csv(f, sep=";")
+        data = data[attributes]
+        data = shuffle(data)
+
+        x = np.array(data.drop([predict], 1))
+        y = np.array(data[predict])
         x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-        linear = linear_model.LinearRegression()
+        best = 0
+        for _ in range(20):
+            x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-        linear.fit(x_train, y_train)
-        acc = linear.score(x_test, y_test)
-        print("Accuracy: " + str(acc))
+            linear = linear_model.LinearRegression()
 
-        if acc > best:
-            best = acc
-            with open("studentgrades.pickle", "wb") as f:
-                pickle.dump(linear, f)
+            linear.fit(x_train, y_train)
+            acc = linear.score(x_test, y_test)
+            print("Accuracy: " + str(acc))
 
-    # LOAD MODEL
-    pickle_in = open("studentgrades.pickle", "rb")
-    linear = pickle.load(pickle_in)
+            if acc > best:
+                best = acc
+                with open("studentgrades.pickle", "wb") as f:
+                    pickle.dump(linear, f)
+
+        # LOAD MODEL
+        pickle_in = open("studentgrades.pickle", "rb")
+        linear = pickle.load(pickle_in)
 
 
-    # print("-------------------------")
-    # print('Coefficient: \n', linear.coef_)
-    # print('Intercept: \n', linear.intercept_)
-    # print("-------------------------")
+        # print("-------------------------")
+        # print('Coefficient: \n', linear.coef_)
+        # print('Intercept: \n', linear.intercept_)
+        # print("-------------------------")
 
-    print(best)
-    predicted= linear.predict(x_test)
-    for x in range(len(predicted)):
-        print(predicted[x], x_test[x], y_test[x])
+        print(best)
+        predicted= linear.predict(x_test)
+        for x in range(len(predicted)):
+            print(predicted[x], x_test[x], y_test[x])
 
-    outputString = "Your model has been trained with {} accuracy.".format(best)
-    output = {
-        'output': outputString
-    }
+        outputString = "Your model has been trained with {} accuracy.".format(best)
+        output = {
+            'output': outputString
+        }
 
-    result = db.reviews.insert_one(output)
+        result = db.reviews.insert_one(output)
+        print(result)
+        collection1.drop()
+        collection2.drop()
 
 if __name__== "__main__":
     main()
